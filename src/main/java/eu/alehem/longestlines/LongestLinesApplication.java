9af83376bc40f10bc5cdc2865d1c140cece1ab13
Alexander Hemberg <alexander.hemberg@gmail.com>
2024-02-01 11:13:57 +0100

    Add base from spring initilizr

---
+++ src/main/java/eu/alehem/longestlines/LongestLinesApplication.java
@@ -0,0 +1,13 @@
+package eu.alehem.longestlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongestLinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongestLinesApplication.class, args);
+	}
+
+}
