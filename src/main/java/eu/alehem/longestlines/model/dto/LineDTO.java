032f1460bc594e0d876e4cc08676f25708563102
Alexander Hemberg <alexander.hemberg@gmail.com>
2024-02-01 11:24:16 +0100

    Reduce LineDTO to minimum needed

--- src/main/java/eu/alehem/longestlines/model/dto/LineDTO.java
+++ src/main/java/eu/alehem/longestlines/model/dto/LineDTO.java
@@ -2,9 +2,4 @@ package eu.alehem.longestlines.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record LineDTO(@JsonProperty("line_number") int lineNumber
-    /*    @JsonProperty("stops_outbound")
-    List<StopDTO> stopsOutbound,
-    @JsonProperty("stops_inbound")
-    List<StopDTO> stopsInbound*/
-    ) {}
+public record LineDTO(@JsonProperty("line_number") int lineNumber) {}
