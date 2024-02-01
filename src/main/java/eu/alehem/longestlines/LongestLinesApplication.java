184a9f3184acfd87f7617245c9a8909e682eae20
Alexander Hemberg <alexander.hemberg@gmail.com>
2024-02-01 12:24:47 +0100

    Codestyle

--- src/main/java/eu/alehem/longestlines/LongestLinesApplication.java
+++ src/main/java/eu/alehem/longestlines/LongestLinesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LongestLinesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LongestLinesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LongestLinesApplication.class, args);
+  }
 }
